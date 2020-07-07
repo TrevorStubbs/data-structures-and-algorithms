a46ed3803b564449731a2da845c3d5fe6a83d253
Trevor Stubbs <stubbste@gmail.com>
2020-07-07 14:47:03 -0700

    Setup for coding

---
+++ Dotnet/code-challenges/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] ArrayShift(int[] inputArray, int inputValue)
+        {
+            int[] newArray = new int[inputArray.Length + 1];
+
+            return newArray;
+        }
+    }
+}
