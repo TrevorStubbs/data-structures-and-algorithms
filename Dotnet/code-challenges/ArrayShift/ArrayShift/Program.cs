1d54c52de54e0d45988aea5bad17f6d0bd3b8c56
Trevor Stubbs <stubbste@gmail.com>
2020-07-07 15:26:34 -0700

    functional test

--- Dotnet/code-challenges/ArrayShift/ArrayShift/Program.cs
+++ Dotnet/code-challenges/ArrayShift/ArrayShift/Program.cs
@@ -6,19 +6,47 @@ namespace ArrayShift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] providedArray1 = new int[] { 1, 4, 6, 8 };
+            int providedValue1 = 5;
+
+            int[] arrayFromMethod1 = InsertShiftArray(providedArray1, providedValue1);
+
+            int[] providedArray2 = new int[] { 4, 8, 15, 23, 42 };
+            int providedValue2 = 16;
+
+            int[] arrayFromMethod2 = InsertShiftArray(providedArray2, providedValue2);
+
+            Console.Write($"First Test: ");
+
+            foreach (int number in arrayFromMethod1)
+                Console.Write($"{number} ");
+
+            Console.WriteLine();
+
+            Console.Write($"Second Test: ");
+            foreach (int number in arrayFromMethod2)
+                Console.Write($"{number} ");
+
         }
 
         public static int[] InsertShiftArray(int[] inputArray, int inputValue)
         {
             int[] newArray = new int[inputArray.Length + 1];
+            int middleIndex;
+
+            middleIndex = newArray.Length / 2;
+            newArray[middleIndex] = inputValue;
 
             for (int i = 0; i < newArray.Length; i++)
             {
-                if (i == newArray.Length - 1)
-                    newArray[i] = inputValue;
-                else
-                    newArray[i] = inputArray[i];
+                    if(i < middleIndex)
+                    {
+                        newArray[i] = inputArray[i];
+                    }
+                    else if (i > middleIndex)
+                    {
+                        newArray[i] = inputArray[i - 1];
+                    }
             }
 
             return newArray;
--- Dotnet/code-challenges/ArrayShift/ArrayShiftTests/UnitTest1.cs
+++ Dotnet/code-challenges/ArrayShift/ArrayShiftTests/UnitTest1.cs
@@ -7,17 +7,33 @@ namespace ArrayShiftTests
     public class UnitTest1
     {
         [Fact]
-        public void CanReturnArrayFromInsertShiftArray()
+        public void ReturnsExpectedArrayFromInsertShiftArray1()
         {
             // arrange
-            int[] testArray = new int[] { 1, 2, 3, 4, 5 };
-            int testValue = 6;
+            int[] testArray = new int[] { 2, 4, 6, 8 };
+            int testValue = 5;
+            int[] expectedArray = new int[] { 2, 4, 5, 6, 8 };
 
             // Act
             int[] newArray = InsertShiftArray(testArray, testValue);
 
             // assert
-            Assert.Equal(6, newArray[5]);
+            Assert.Equal(expectedArray, newArray);
+        }
+
+        [Fact]
+        public void ReturnsExpectedArrayFromInsertShiftArray2()
+        {
+            // arrange
+            int[] testArray = new int[] { 4, 8, 15, 23, 42 };
+            int testValue = 16;
+            int[] expectedArray = new int[] { 4, 8, 15, 16, 23, 42 };
+
+            // Act
+            int[] newArray = InsertShiftArray(testArray, testValue);
+
+            // assert
+            Assert.Equal(expectedArray, newArray);
         }
     }
 }
