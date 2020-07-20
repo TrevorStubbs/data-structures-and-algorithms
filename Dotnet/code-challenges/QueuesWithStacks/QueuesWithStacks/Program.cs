b5d724ad0cc3287937ff0f875aa23a4c69bcc497
Trevor Stubbs <stubbste@gmail.com>
2020-07-20 15:49:03 -0700

    setup for code challenge

---
+++ Dotnet/code-challenges/QueuesWithStacks/QueuesWithStacks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueuesWithStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
