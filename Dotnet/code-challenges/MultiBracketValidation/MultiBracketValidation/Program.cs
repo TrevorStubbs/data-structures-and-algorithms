af0b3922eccb88d3a7c13f3225a42ea4ccf5513a
Trevor Stubbs <stubbste@gmail.com>
2020-07-22 16:37:57 -0700

    Setup for answers

---
+++ Dotnet/code-challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using StacksAndQueuesLibrary;
+
+namespace MultiBracketValidation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(MultiBracketValidation("{}"));
+            Console.WriteLine(MultiBracketValidation("{}(){}"));
+            Console.WriteLine(MultiBracketValidation("{}()[[Extra Characters]]"));
+            Console.WriteLine(MultiBracketValidation("(){}[[]]"));
+            Console.WriteLine(MultiBracketValidation("{}{Code}[Fellows](())"));
+            Console.WriteLine(MultiBracketValidation("[({}]"));
+            Console.WriteLine(MultiBracketValidation("(]("));
+            Console.WriteLine(MultiBracketValidation("{(})"));
+
+        }
+
+        public static bool MultiBracketValidation(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+            Queue<char> que = new Queue<char>();
+
+            foreach (var item in input)
+            {
+                if (item == '{' || item == '[' || item == '(')
+                    stack.Push(item);
+                else if (item == '}' || item == ']' || item == ')')
+                    que.Enqueue(item);
+            }
+
+            while(!stack.IsEmpty() && !que.IsEmpty())
+            {
+                char leftChar = stack.Pop().Value;
+                char rightChar = que.Dequeue().Value;
+
+                if (leftChar == '{')
+                    if (rightChar != '}')
+                        return false;
+                else if (leftChar == '[')
+                    if (rightChar != ']')
+                        return false;
+                else if (leftChar == '(')
+                    if (rightChar != ')')
+                        return false;
+            }
+
+            if (!stack.IsEmpty() || !que.IsEmpty())
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Dotnet/code-challenges/MultiBracketValidation/README.md
@@ -0,0 +1,53 @@
+## Multi Bracket Validation
+*Author: Trevor Stubbs*
+
+---
+
+### Problem Domain
+Your function should take a string as its only argument, and should return a boolean representing whether or not the brackets in the string are balanced. There are 3 types of brackets:
+
+- Round Brackets : ()
+- Square Brackets : []
+- Curly Brackets : {}
+
+---
+
+### Inputs and Expected Outputs
+
+| Input1 | Expected Output |
+| :----------- | :----------- |
+| {}	| TRUE
+| {}(){} |	TRUE
+| ()[[Extra Characters]] |	TRUE
+| (){}[[]] |	TRUE
+| {}{}(()) |	TRUE
+| [({}] |	FALSE
+| (](	| FALSE
+| {(})	| FALSE
+
+
+---
+
+### Big O
+
+
+| Time | Space |
+| :----------- | :----------- |
+| O(n) | O(n) |
+
+
+---
+
+
+### Whiteboard Visual
+![WhiteBoard](assets/whiteboard.png)
+
+
+---
+
+### Change Log
+- 1.0 Repo Setup 
+
+---
+
+For more information on Markdown: https://www.markdownguide.org/cheat-sheet
