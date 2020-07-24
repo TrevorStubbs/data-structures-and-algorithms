662a2215af44813451433be47de064f5988f7f86
Trevor Stubbs <stubbste@gmail.com>
2020-07-24 16:25:38 -0700

    BST started

--- Dotnet/data-structures/TreesLibrary/TreesLibrary/Tree.cs
+++ Dotnet/data-structures/TreesLibrary/TreesLibrary/Tree.cs
@@ -8,17 +8,29 @@ namespace TreesLibrary
     {
         public Node<T> Root { get; set; }
 
+        /// <summary>
+        /// Default constructor to build and empty Tree
+        /// </summary>
         public Tree()
         {
 
         }
 
+        /// <summary>
+        /// Constructor to build a new tree with a defined root node
+        /// </summary>
+        /// <param name="value">Take a parameter of the instantiation defined type</param>
         public Tree(T value)
         {
             Node<T> root = new Node<T>(value);
             Root = root;
         }
 
+        /// <summary>
+        /// Move through the tree in a PreOrder (root first) fashion
+        /// </summary>
+        /// <param name="root">Take a parameter of the instantiation defined type</param>
+        /// <returns>A list of the ordered nodes</returns>
         public List<T> PreOrder(Node<T> root)
         {
             List<T> traversal = new List<T>();
@@ -27,6 +39,11 @@ namespace TreesLibrary
             return traversal;
         }
 
+        /// <summary>
+        /// Helper method for the PreOrder Method. Does the recursion.
+        /// </summary>
+        /// <param name="traversal">Takes the list defined by PreOrder above</param>
+        /// <param name="root">Take the node of the root of the tree</param>
         private void PreOrder(List<T> traversal, Node<T> root)
         {
             traversal.Add(root.Value);
@@ -37,6 +54,11 @@ namespace TreesLibrary
                 PreOrder(traversal, root.RightChild);
         }
 
+        /// <summary>
+        /// Move through the tree in a InOrder (root in the middle) fashion
+        /// </summary>
+        /// <param name="root">Take a parameter of the instantiation defined type</param>
+        /// <returns>A list of the ordered nodes</returns>
         public List<T> InOrder(Node<T> root)
         {
             List<T> traversal = new List<T>();
@@ -45,6 +67,11 @@ namespace TreesLibrary
             return traversal;
         }
 
+        /// <summary>
+        /// Helper method for the InOrder Method. Does the recursion.
+        /// </summary>
+        /// <param name="traversal">Takes the list defined by InOrder above</param>
+        /// <param name="root">Take the node of the root of the tree</param>
         private void InOrder(List<T> traversal, Node<T> root)
         {
             if (root.LeftChild != null)
@@ -56,6 +83,11 @@ namespace TreesLibrary
                 InOrder(traversal, root.RightChild);
         }
 
+        /// <summary>
+        /// Move through the tree in a PostOrder (root last) fashion
+        /// </summary>
+        /// <param name="root">Take a parameter of the instantiation defined type</param>
+        /// <returns>A list of the ordered nodes</returns>
         public List<T> PostOrder(Node<T> root)
         {
             List<T> traversal = new List<T>();
@@ -64,6 +96,11 @@ namespace TreesLibrary
             return traversal;
         }
 
+        /// <summary>
+        /// Helper method for the PostOrder Method. Does the recursion.
+        /// </summary>
+        /// <param name="traversal">Takes the list defined by PostOrder above</param>
+        /// <param name="root">Take the node of the root of the tree</param>
         private void PostOrder(List<T> traversal, Node<T> root)
         {
             if (root.LeftChild != null)
