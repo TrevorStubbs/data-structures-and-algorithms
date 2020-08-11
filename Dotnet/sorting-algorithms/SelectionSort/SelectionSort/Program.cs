cf24eaa4b36b6a6db4d42237d47c002d78c15032
Trevor Stubbs <stubbste@gmail.com>
2020-08-10 20:46:07 -0700

    Setup for answers

---
+++ Dotnet/sorting-algorithms/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace InsertionSortProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {            
+            int[] input1 = { 20, 18, 12, 8, 5, -2 };
+            int[] input2 = { 5, 12, 7, 5, 5, 7 };
+            int[] input3 = { 2, 3, 5, 7, 13, 11 };
+
+            int[] output = InsertionSortMethod(input1);
+
+            foreach (var item in output)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            output = InsertionSortMethod(input2);
+
+            foreach (var item in output)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            output = InsertionSortMethod(input3);
+
+            foreach (var item in output)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        public static int[] InsertionSortMethod(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int j = i - 1;
+                int temp = arr[i];
+
+                while (j >= 0 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+
+                arr[j + 1] = temp;
+            }
+
+            return arr;
+        }
+    }
+}
