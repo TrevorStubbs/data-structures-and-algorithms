4abbfc8e2fe19f2e6d84cb8fa9fdc7c1e399f951
Trevor Stubbs <stubbste@gmail.com>
2020-09-14 15:51:17 -0700

    leet code problems

---
+++ interview-problems/IsAnIntAPalindrome/IsAnIntAPalindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsAnIntAPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnIntAPalindrome(-121));
+        }
+
+        public static bool IsAnIntAPalindrome(int inputInt)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            string str = inputInt.ToString();
+
+            foreach (var item in str)
+            {
+                stack.Push(item);
+            }
+
+            foreach (var item in str)
+            {
+                if (stack.TryPeek(out _))
+                {
+                    if(stack.Pop() != item)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ interview-problems/ReverseInteger/ReverseInteger/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWithStringManipulation(-123));
+        }
+
+        public static int ReverseWithStringManipulation(int inputInt)
+        {
+            List<char> charList = inputInt.ToString().ToCharArray().ToList();
+
+            char negative = default;
+
+            if (charList[0] == '-')
+            {
+                charList.Remove(charList[0]);
+                negative = '-';
+            }
+
+            charList.Reverse();
+
+            if (negative != default)
+            {
+                charList.Insert(0, '-');
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var character in charList)
+            {
+                sb.Append(character.ToString());
+            }
+
+            string outputStr = sb.ToString();
+
+            bool tryThis = Int32.TryParse(outputStr, out int outputInt);
+
+            if (!tryThis)
+                return 0;
+
+            return outputInt;
+        }
+    }
+}
+
