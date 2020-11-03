eef1cdecdb3e1d148aa3a3b66d5d019d9b89206e
Trevor Stubbs <stubbste@gmail.com>
2020-11-02 21:32:56 -0800

    git add longest substring

---
+++ interview-problems/LongestSubsringWithoutRepeatingChars/LongestSubsringWithoutRepeatingChars/Classes/RepeatingSubstring.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongestSubsring.Classes
+{
+    public static class RepeatingSubstring
+    {
+        public static int LongestSubstringWithoutRepeats(string inputString)
+        {
+            char[] charArray = inputString.ToCharArray();
+            List<char> charList = new List<char>();
+            int counter = 0;
+            int subCounter = 0;
+
+            if (charArray.Length == 0)
+            {
+                return 0;
+            }
+
+            foreach (var character in charArray)
+            {
+                if (charList.Count == 0)
+                {
+                    charList.Add(character);
+                    counter = 1;
+                    subCounter = 1;
+                }
+                else
+                {
+                    if (charList.Contains(character))
+                    {
+                        if (counter < charList.Count)
+                        {
+                            counter = charList.Count;
+                        }
+
+                        charList.Clear();
+
+                        charList.Add(character);
+                        subCounter = 0;
+                    }
+                    else
+                    {
+                        charList.Add(character);
+                        subCounter++;
+                    }
+                }
+            }
+
+            if (counter < charList.Count)
+            {
+                counter = charList.Count;
+            }
+
+            if (subCounter > counter)
+            {
+                return subCounter;
+            }
+            else
+            {
+                return counter;
+            }
+
+        }
+    }
+}
---
+++ interview-problems/LongestSubsringWithoutRepeatingChars/LongestSubsringWithoutRepeatingChars/Program.cs
@@ -0,0 +1,27 @@
+﻿using LongestSubsring.Classes;
+using System;
+
+namespace LongestSubsring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr1 = "abcabcbb"; // Expected: 3
+            string inputStr2 = "bbbb"; // Expected: 1
+            string inputStr3 = "pwwkew"; // Expected: 3
+            string inputStr4 = "au";
+            string inputStr5 = "aab";
+
+            Console.WriteLine($"Expected: 3 Answer: {RepeatingSubstring.LongestSubstringWithoutRepeats(inputStr1)}");
+            Console.WriteLine();
+            Console.WriteLine($"Expected: 1 Answer: {RepeatingSubstring.LongestSubstringWithoutRepeats(inputStr2)}");
+            Console.WriteLine();
+            Console.WriteLine($"Expected: 3 Answer: {RepeatingSubstring.LongestSubstringWithoutRepeats(inputStr3)}");
+            Console.WriteLine();
+            Console.WriteLine($"Expected: 2 Answer: {RepeatingSubstring.LongestSubstringWithoutRepeats(inputStr4)}");
+            Console.WriteLine();
+            Console.WriteLine($"Expected: 2 Answer: {RepeatingSubstring.LongestSubstringWithoutRepeats(inputStr5)}");
+        }
+    }
+}
