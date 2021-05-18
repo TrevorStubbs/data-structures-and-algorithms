5e73dee4e18eb7556bc76c29f574980e93a29866
Trevor Stubbs <stubbste@gmail.com>
2021-05-17 18:34:28 -0700

    Find the median of 2 sorted arrays. Complete.

---
+++ interview-problems/FindTheMedianOf2Arrays/FindTheMedianOf2Arrays/FindTheMedian.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FindTheMedianOf2Arrays
+{
+    public static class FindTheMedian
+    {
+        public static double FindMedianSortedArrays(int[] array1, int[] array2)
+        {
+            //if (array1.Length == 0 || array2.Length == 0)
+            //    return 0;
+
+            var newList = array1.Concat(array2).ToList();
+            newList.Sort();
+
+            return FindMedian(newList);
+        }
+
+        private static double FindMedian(List<int> inputList)
+        {
+            double output = 0;
+
+            if (inputList.Count % 2 == 0)
+            {
+                int index = inputList.Count / 2;
+                output = ((double)inputList[index - 1] + (double)inputList[index]) / 2;
+            }
+            else
+            {
+                int index = inputList.Count / 2;
+                output = inputList[index];
+            }
+
+            return output;
+        }
+    }
+}
---
+++ interview-problems/FindTheMedianOf2Arrays/FindTheMedianOf2Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FindTheMedianOf2Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] array1 = {1, 2};
+            int[] array2 = {3, 4};
+
+            var answer = FindTheMedian.FindMedianSortedArrays(array1, array2);
+
+            Console.WriteLine($"The answer is: {answer}.");
+
+        }
+    }
+}
