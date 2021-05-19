55dba6ea118d2c13d2e13ecc312fdd40dc535c9d
Trevor Stubbs <stubbste@gmail.com>
2021-05-19 09:32:25 -0700

    Continued work

---
+++ interview-problems/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RemoveDuplicatesFromSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+
+            int[] inputArray = { 0, 1, 1, 2, 2, 3, 5, 6, 6 };
+
+            var ouput = SortedArrayMethods.RemoveDuplicates(inputArray);
+
+            foreach (var item in ouput)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
---
+++ interview-problems/RemoveDuplicatesFromSortedArray/SortedArrayMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoveDuplicatesFromSortedArray
+{
+    public static class SortedArrayMethods
+    {
+        public static int[] RemoveDuplicates(int[] inputNumbers)
+        {
+            var arrayLength = inputNumbers.Length;
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if (inputNumbers[i + 1] != default)
+                {
+                    if (inputNumbers[i] == inputNumbers[i + 1])
+                    {
+                        for (int j = i + 2; j < arrayLength; j++)
+                        {
+                            inputNumbers[j - 1] = inputNumbers[j];
+
+                            if (j == arrayLength-1)
+                                inputNumbers[j] = default;
+                        }
+
+                        arrayLength--;
+                    }
+                }
+            }
+
+            return inputNumbers;
+        }
+    }
+}
