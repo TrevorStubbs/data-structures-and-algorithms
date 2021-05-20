ce15263247adb4230084ea8413e4ea620f21211d
Trevor Stubbs <stubbste@gmail.com>
2021-05-19 21:43:44 -0700

    playground

---
+++ interview-problems/AmazonPlayground/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AmazonPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string word = "bag";
+            string words = "baggage";
+
+            Console.WriteLine(word[2]);
+
+            var split = word.Split(word[2]);
+            Console.WriteLine(split[0]);
+
+              
+            Console.WriteLine(words.Contains(word));
+
+        }
+    }
+}
