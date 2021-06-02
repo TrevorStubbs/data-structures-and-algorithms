bf87c61a609c45810cd695017e50ecbe6c0a0386
Trevor Stubbs <stubbste@gmail.com>
2021-06-02 09:41:02 -0700

    Sliding Windows Challenge 1 complete.

--- CodingPatterns/Patterns.md
+++ CodingPatterns/Patterns.md
@@ -6,6 +6,61 @@
 - The sliding window keeps track of the common elements from each pass 
     - Only add the new element and subtract the element no longer needed.
 - Turns O(k*n) -> O(n)
+
+``` CSharp
+// While Loop
+public static int UsingAWhileLoop(List<int> inputList)
+{
+
+    // Figure out the size of the window needs to be. 
+    // Is it variable? How is it going to vary? 
+    int windowSize = 5; 
+
+    // These are the iterators for the window
+    int windowEnd = 0; 
+    int windowStart = 0;
+    while(windowEnd < inputList.Count)
+    {
+        // Do something with the element at "windowEnd"
+        something = inputList[windowEnd]
+
+
+        // Figure out how to keep the window at the appropriate size. 
+        if((windowEnd - windowStart) > windowSize)
+        {
+            // Do something with the beginning of the window.
+            something -= inputList[windowStart];
+            // Move the windowStart pointer.
+            windowStart++;
+        }
+
+        // Iterate the windowEnd
+        windowEnd++;
+    }
+}
+
+// For Loop
+public static int UsingAForLoop(List<int> inputList)
+{
+    int windowSize = 5;
+    int windowStart = 0;
+
+    for(int windowEnd = 0; windowEnd < inputList.Count; windowEnd++)
+    {
+        // Do something with the element at "windowEnd"
+        something = inputList[windowEnd];
+
+        if((windowEnd - windowStart) > windowSize)
+        {
+            // Do something with the beginning of the window.
+            something -= inputList[windowStart];
+            // Move the windowStart pointer.
+            windowStart++;
+        }
+    }
+}
+
+```
 ## Two Pointer
 ## Fast & Slow
 ## Merge Intervals
--- CodingPatterns/SlidingWindow/SlidingWindow/Program.cs
+++ CodingPatterns/SlidingWindow/SlidingWindow/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace SlidingWindow
@@ -29,11 +30,56 @@ namespace SlidingWindow
 
             var windowSum = SmallestSubarray.SmallestSubarrayGreaterThanS(8, new int[] { 3, 4, 1, 1, 6 });
 
-            Console.Write($"{windowSum}");
+            Console.Write($"WindowSum: {windowSum}");
+
+            Console.WriteLine();
+
+            var distinct = LongestSubstring.LongestSubstringWithNoMoreThanKDistinctChars("cbbbebi", 1);
+
+            Console.WriteLine($"Longest Substring: {distinct}");
+
+
+            Console.WriteLine();
+
+            var fruits = FruitTree.MaxNoOfFruits(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' });
+
+            Console.WriteLine($"Fruits: {fruits}");
+
+            Console.WriteLine();
+
+            var maxNoRepeats = NoRepeatChars.NoRepeatCharsInASubstring("abccde");
+
+            Console.WriteLine($"Max No Repeats: {maxNoRepeats}");
+
+            Console.WriteLine();
+
+            var longestSubstring = ReplaceToFindLongest.ReplaceKCharsToFindLongestSubString("aabccbb", 2);
+
+            Console.WriteLine($"Replace to find longest: {longestSubstring}");
+
+            Console.WriteLine();
+
+            var replaceBits = BitArrayReplace.ReplaceK1sInBitArray(new List<int>() { 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1 }, 3);
+
+            Console.WriteLine($"Longest Replaced 0s: {replaceBits}");
+
+            Console.WriteLine();
+
+            var isPermutation = ContainsPermuations.DoesContainPermutation("oidbcaf", "abc");
+
+            Console.WriteLine($"Is Permutation: {isPermutation}");
+
+            Console.WriteLine();
+
+            var isEducativePermutation = ContainsPermuations.EducativeVersion("oidbcaf", "abc");
+
+            Console.WriteLine($"Is Permutation: {isEducativePermutation}");
+
+            Console.WriteLine();
         }
 
         // Find the average of all contiguous subarrays of size 'K'.
-        static class AverageOfSubarrayOfSizeK
+        public static class AverageOfSubarrayOfSizeK
         {
             // Brute Force Method -
             // Time Complexity O(K*N) where N is the number of elements in the array.
@@ -103,7 +149,7 @@ namespace SlidingWindow
                         if (windowSum >= s)
                             return windowSize;
 
-                        if(windowEnd >= windowSize - 1)
+                        if (windowEnd >= windowSize - 1)
                         {
                             windowSum -= window[0];
                             window.RemoveAt(0);
@@ -119,31 +165,315 @@ namespace SlidingWindow
 
         public static class LongestSubstring
         {
+            // Time - O(n)
+            // Space - O(k) 
             public static int LongestSubstringWithNoMoreThanKDistinctChars(string input, int k)
             {
-                Dictionary<char, int> charDict = new Dictionary<char, int>();
+                HashSet<char> window = new HashSet<char>();
 
-                int outputNumber = 0;
+                // This is the output.
+                int maxLength = 0;
 
-                List<char> window = new List<char>();
+                int windowEnd = 0;
+                int windowStart = 0;
 
-                foreach (var ch in input)
+                while (windowEnd < input.Length)
                 {
-                    if(!charDict.ContainsKey(ch))
+                    // If the char at "windowEnd" is not in the hashset then add it.
+                    if (!window.Contains(input[windowEnd]))
                     {
-                        charDict.Add(ch, 0);
+                        window.Add(input[windowEnd]);
                     }
 
-                    charDict[ch]++;
-
-                    if (charDict.Count == k)
+                    // While the hashset is larger than "k" number of distinct characters then keep removing chars until the length of the window is less than or equal to "k".
+                    // Every time we remove an item from the window iterate "windowStart" to shrink the window.
+                    while (window.Count > k)
                     {
-                        break;
+                        window.Remove(input[windowStart]);
+                        windowStart++;
+                    }
+
+                    maxLength = Math.Max(maxLength, (windowEnd - windowStart + 1));
+
+                    windowEnd++;
+                }
+
+                return maxLength;
+            }
+        }
+
+        public static class FruitTree
+        {
+            // Time O(n) - O(n+n) If it gets triggered the inner loop is processed only once.
+            // Space O(1) - constant space
+            public static int MaxNoOfFruits(char[] charArray)
+            {
+                HashSet<char> window = new HashSet<char>();
+
+                int maxFruits = 0;
+                int windowStart = 0;
+
+                for (int windowEnd = 0; windowEnd < charArray.Length; windowEnd++)
+                {
+                    if (!window.Contains(charArray[windowEnd]))
+                    {
+                        window.Add(charArray[windowEnd]);
+                    }
+
+                    while (window.Count > 2)
+                    {
+                        window.Remove(charArray[windowStart]);
+                        windowStart++;
+                    }
+
+                    maxFruits = Math.Max(maxFruits, (windowEnd - windowStart + 1));
+                }
+
+                return maxFruits;
+
+            }
+        }
+
+        public static class NoRepeatChars
+        {
+            public static int NoRepeatCharsInASubstring(string inputString)
+            {
+                // This time we need a Dictionary to keep track of the char's index in the string.
+                Dictionary<char, int> indexMap = new Dictionary<char, int>();
+
+                // Same as above
+                int maxLength = 0;
+                int windowStart = 0;
+
+                for (int windowEnd = 0; windowEnd < inputString.Length; windowEnd++)
+                {
+                    // If the Dictionary contains the key then we will move the windowStart to the farthest right repeat char.  
+                    if (indexMap.ContainsKey(inputString[windowEnd]))
+                    {
+                        // We add 1 due to iterators starting at 0.
+                        windowStart = Math.Max(windowStart, indexMap[inputString[windowEnd]] + 1);
+                        indexMap[inputString[windowEnd]] = windowEnd;
+                    }
+                    else
+                    {
+                        // If indexMap doesn't have the char then it is distinct so we should add it to the Dictionary.
+                        indexMap.Add(inputString[windowEnd], windowEnd);
+                    }
+
+                    maxLength = Math.Max(maxLength, (windowEnd - windowStart + 1));
+
+                }
+
+                return maxLength;
+            }
+        }
+
+        public static class ReplaceToFindLongest
+        {
+            public static int ReplaceKCharsToFindLongestSubString(string inputString, int k)
+            {
+                // We need a dictionary to record the number of times a char is repeated
+                Dictionary<char, int> freqDict = new Dictionary<char, int>();
+
+                int maxLength = 0;
+                int windowStart = 0;
+
+                // Keep track to the longest repeated character
+                int maxRepeatLetterCount = 0;
+
+                for (int windowEnd = 0; windowEnd < inputString.Length; windowEnd++)
+                {
+                    // If the char is not in the dictionary then add it with a value of 0.
+                    if (!freqDict.ContainsKey(inputString[windowEnd]))
+                    {
+                        freqDict.Add(inputString[windowEnd], 0);
+                    }
+
+                    // No matter what add 1 to that char's counter.
+                    freqDict[inputString[windowEnd]] += 1;
+
+                    // Check to see which if the char at window end has more repeats than the previous longest.
+                    maxRepeatLetterCount = Math.Max(maxRepeatLetterCount, freqDict[inputString[windowEnd]]);
+
+                    // Determine if we need to shrink the window.
+                    // ie - 3 - 1 + 1 + 4
+                    // This doesn't need to be a while loop since we have only added 1 char to the Dict.
+                    if ((windowEnd - windowStart + 1 - maxRepeatLetterCount) > k)
+                    {
+                        // Remove 1 from the char at index windowStart
+                        freqDict[inputString[windowStart]] -= 1;
+                        // Move window start 1 spot to the right. 
+                        windowStart += 1;
+                    }
+
+                    // Check to see if this window is biggest window.
+                    maxLength = Math.Max(maxLength, windowEnd - windowStart + 1);
+                }
+
+                return maxLength;
+            }
+        }
+
+        public static class BitArrayReplace
+        {
+            public static int ReplaceK1sInBitArray(List<int> inputArray, int k)
+            {
+                Dictionary<int, int> freqDict = new Dictionary<int, int>();
+
+                int windowStart = 0;
+                int maxLength = 0;
+
+                // Find a window with k number of 0s. and that will be compared to maxLength.
+                for (int windowEnd = 0; windowEnd < inputArray.Count; windowEnd++)
+                {
+                    if (!freqDict.ContainsKey(inputArray[windowEnd]))
+                    {
+                        freqDict.Add(inputArray[windowEnd], 0);
+                    }
+
+                    freqDict[inputArray[windowEnd]] += 1;
+
+                    while (freqDict[0] > k)
+                    {
+                        freqDict[inputArray[windowStart]] -= 1;
+                        windowStart += 1;
+                    }
+
+                    maxLength = Math.Max(maxLength, windowEnd - windowStart + 1);
+                }
+
+                return maxLength;
+            }
+        }
+
+        public static class ContainsPermuations
+        {
+            public static bool DoesContainPermutation(string inputString, string pattern)
+            {
+                // Build a Dictionary that has all the Chars as the keys and the values are the number of times those chars appear in the input pattern.
+                var permutationDict = GeneratePermutationDict(pattern);
+
+                // A list of chars that represents the window.
+                List<char> window = new List<char>();
+
+                // Keep track of the start position
+                int windowStart = 0;
+
+                for (int windowEnd = 0; windowEnd < inputString.Length; windowEnd++)
+                {
+                    // add the last char of the input to the window
+                    window.Add(inputString[windowEnd]);
+
+                    // if the window is larger than the length of the pattern then remove the first element from the window.
+                    if (windowEnd - windowStart + 1 > pattern.Length)
+                    {
+                        window.Remove(inputString[windowStart]);
+                        // Move the start point 1 spot to the right.
+                        windowStart++;
+                    }
+
+                    // if the window is the size of the pattern then start the comparison. 
+                    if (window.Count == pattern.Length)
+                    {
+                        // If the window matches a permutation then return true.
+                        if (DoesContainPerm(window, permutationDict))
+                            return true;
                     }
                 }
 
-                return 0;
+                return false;
             }
+
+            private static Dictionary<char, int> GeneratePermutationDict(string pattern)
+            {
+                Dictionary<char, int> freqDict = new Dictionary<char, int>();
+
+                // Go through each character and count how many times it shows up in the pattern and add that number to the Dict
+                foreach (var ch in pattern)
+                {
+                    if (!freqDict.ContainsKey(ch))
+                        freqDict.Add(ch, 0);
+
+                    freqDict[ch] += 1;
+                }
+
+                return freqDict;
+            }
+
+            private static bool DoesContainPerm(List<char> listOfChars, Dictionary<char, int> freqDict)
+            {
+                // Build a temp dict so we are not changing the original dict.
+                var tempDict = new Dictionary<char, int>(freqDict);
+
+                // Go through the list of characters and see if they match the permutation dictionary.          
+                foreach (var ch in listOfChars)
+                {
+                    // If the char is not in the dict then just return false cause the rest will not be true.
+                    if (!tempDict.ContainsKey(ch))
+                        return false;
+                    else
+                        // Subtract 1 from that dict
+                        tempDict[ch]--;
+                }
+
+                // Here we are checking to see if all the values of the keys are 0. If they are then we can return true.
+                foreach (var ch in tempDict)
+                {
+                    if (ch.Value != 0)
+                        return false;
+                }
+
+                return true;
+            }
+            public static bool EducativeVersion(string str, string pattern)
+            {
+                int windowStart = 0, matched = 0;
+
+                // The example has the same statements but all in this method (no need to rewrite it).
+                Dictionary<char, int> charFreqDict = GeneratePermutationDict(pattern);
+
+                // Everything is done in place. No multiple loops.
+                for (int windowEnd = 0; windowEnd < str.Length; windowEnd++)
+                {
+                    // If char is in the Dict
+                    if (charFreqDict.ContainsKey(str[windowEnd]))
+                    {
+                        //...If so then subtract that 1 from that key.
+                        charFreqDict[str[windowEnd]] -= 1;
+
+                        // If char's counter is 0 then add 1 to the match variable
+                        if (charFreqDict[str[windowEnd]] == 0)
+                            matched++;
+                    }
+
+
+                    // If match number matches the count of the dict then we know that this window is a permutation of the pattern.
+                    if (matched == charFreqDict.Count)
+                        return true;
+
+                    // Move windowStart if the window becomes larger than pattern.
+                    if(windowEnd >= pattern.Length - 1)
+                    {
+                        // Variable for clarity
+                        var leftChar = str[windowStart++];
+
+                        // If str[windowStart] is in the dictionary then revert what we did above.
+                        if (charFreqDict.ContainsKey(leftChar))
+                        {
+                            // Take 1 away from matched
+                            if (charFreqDict[leftChar] == 0)
+                                matched--;
+
+                            // Add 1 back to the key's counter.
+                            charFreqDict[leftChar] += 1;
+                        }
+                    }
+                }
+
+                return false;
+            }
+
         }
+
     }
 }
