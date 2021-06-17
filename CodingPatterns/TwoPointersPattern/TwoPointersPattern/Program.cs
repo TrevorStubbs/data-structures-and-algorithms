c7004f9b456628adf92d6b05db10bc9b8489f9ca
Trevor Stubbs <stubbste@gmail.com>
2021-06-17 10:24:07 -0700

    quad fin

--- CodingPatterns/TwoPointersPattern/TwoPointersPattern/Program.cs
+++ CodingPatterns/TwoPointersPattern/TwoPointersPattern/Program.cs
@@ -802,7 +802,54 @@ namespace TwoPointersPattern
 
             public static List<List<int>> ListOfQuadsEducative(int[] arr, int target)
             {
+                Array.Sort(arr);
+                List<List<int>> quads = new List<List<int>>();
 
+                for (int i = 0; i < arr.Length - 3; i++)
+                {
+                    if (i > 0 && arr[i] == arr[i - 1])
+                        continue;
+                    for (int j = i + 1; j < arr.Length - 2; j++)
+                    {
+                        if (j > i + 1 && arr[j] == arr[j - 1])
+                            continue;
+                        SearchPairs(arr, target, i, j, quads);
+                    }
+                }
+
+                return quads;
+            }
+
+            private static void SearchPairs(int[] arr, int targetSum, int first, int second, List<List<int>> quads)
+            {
+                int left = second + 1;
+                int right = arr.Length - 1;
+                while (left > right)
+                {
+                    int sum = arr[first] + arr[second] + arr[left] + arr[right];
+
+                    if (sum == targetSum)
+                    {
+                        quads.Add(new List<int>()
+                        {
+                            arr[first],
+                            arr[second],
+                            arr[left],
+                            arr[right]
+                        });
+
+                        left++;
+                        right--;
+                        while (left < right && arr[left] == arr[left - 1])
+                            left++;
+                        while (left < right && arr[right] == arr[right + 1])
+                            right--;
+                    }
+                    else if (sum < targetSum)
+                        left++;
+                    else
+                        right--;
+                }
             }
         }
 
