982fb317a20e10ccb326b9260a8ab30ca1835f0f
Trevor Stubbs <stubbste@gmail.com>
2021-07-01 09:00:09 -0700

    Merge Intervals Start

---
+++ CodingPatterns/MergeIntervalsPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MergeIntervalsPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
