925a2764a8a30a2b130af898499986cb602ab84d
Trevor Stubbs <stubbste@gmail.com>
2021-07-07 09:54:27 -0700

    intervals 2

--- CodingPatterns/MergeIntervalsPattern/Program.cs
+++ CodingPatterns/MergeIntervalsPattern/Program.cs
@@ -9,15 +9,70 @@ namespace MergeIntervalsPattern
     {
         static void Main(string[] args)
         {
-            List<Interval> input = new List<Interval>();
-            input.Add(new Interval(7, 9));
-            input.Add(new Interval(1, 4));
-            input.Add(new Interval(2, 5));
+            List<Interval> input1 = new List<Interval>();
+            input1.Add(new Interval(7, 9));
+            input1.Add(new Interval(1, 4));
+            input1.Add(new Interval(2, 5));
 
-            var merged = MergeIntervals.MergeOverlapingIntervals(input);
+            var merged1 = MergeIntervals.MergeIntervalsWithoutEnumerator(input1);
 
-            foreach (var interval in merged)            
-                Console.Write($"[{interval.Start}, {interval.End}] ");            
+            foreach (var interval in merged1)
+                Console.Write($"[{interval.Start}, {interval.End}] ");
+
+            Console.WriteLine();
+
+            List<Interval> input2 = new List<Interval>();
+            input2.Add(new Interval(6, 7));
+            input2.Add(new Interval(2, 4));
+            input2.Add(new Interval(5, 9));
+
+            var merged2 = MergeIntervals.MergeIntervalsWithoutEnumerator(input2);
+
+            foreach (var interval in merged2)
+                Console.Write($"[{interval.Start}, {interval.End}] ");
+
+            Console.WriteLine();
+
+            List<Interval> input3 = new List<Interval>();
+            input3.Add(new Interval(1, 4));
+            input3.Add(new Interval(2, 6));
+            input3.Add(new Interval(3, 5));
+
+            var merged3 = MergeIntervals.MergeIntervalsWithoutEnumerator(input3);
+
+            foreach (var interval in merged3)
+                Console.Write($"[{interval.Start}, {interval.End}] ");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            List<Interval> insertTest1 = new List<Interval>();
+            insertTest1.Add(new Interval(1, 3));
+            insertTest1.Add(new Interval(5, 7));
+            insertTest1.Add(new Interval(8, 12));
+
+            var insertMerge1 = MergeIntervals.InsertInterval(insertTest1, new Interval(4, 6));
+
+            foreach (var interval in insertMerge1)
+                Console.Write($"[{interval.Start}, {interval.End}] ");
+
+            Console.WriteLine();
+
+            var insertMerge2 = MergeIntervals.InsertInterval(insertTest1, new Interval(4, 10));
+
+            foreach (var interval in insertMerge2)
+                Console.Write($"[{interval.Start}, {interval.End}] ");
+
+            Console.WriteLine();
+
+            List<Interval> insertTest3 = new List<Interval>();
+            insertTest3.Add(new Interval(2, 3));
+            insertTest3.Add(new Interval(5, 7));
+
+            var insertMerge3 = MergeIntervals.InsertInterval(insertTest3, new Interval(1, 4));
+
+            foreach (var interval in insertMerge3)
+                Console.Write($"[{interval.Start}, {interval.End}] ");
 
             Console.WriteLine();
 
@@ -25,6 +80,8 @@ namespace MergeIntervalsPattern
 
         public static class MergeIntervals
         {
+            // Time: O(n * logN)
+            // Space: O(n)
             public static List<Interval> MergeOverlapingIntervals(List<Interval> intervals)
             {
                 // If there is only 1 interval then just return the input.
@@ -62,6 +119,59 @@ namespace MergeIntervalsPattern
                 mergedIntervals.Add(new Interval(start, end));
 
                 return mergedIntervals;
+            }
+
+            public static List<Interval> MergeIntervalsWithoutEnumerator(List<Interval> intervals)
+            {
+                if (intervals.Count < 2)
+                    return intervals;
+
+                intervals = intervals.OrderBy(x => x.Start).ToList();
+
+                List<Interval> mergedIntervals = new List<Interval>();
+                int start = intervals[0].Start, end = intervals[0].End;
+
+                foreach (var interval in intervals)
+                {
+                    if (interval.Start <= end)
+                        end = Math.Max(interval.End, end);
+                    else
+                    {
+                        mergedIntervals.Add(new Interval(start, end));
+                        start = interval.Start;
+                        end = interval.End;
+                    }
+                }
+
+                mergedIntervals.Add(new Interval(start, end));
+
+                return mergedIntervals;
+            }
+
+            public static List<Interval> InsertInterval(List<Interval> intervals, Interval newInterval)
+            {
+                intervals.Add(newInterval);
+
+                intervals = intervals.OrderBy(x => x.Start).ToList();
+
+                List<Interval> mergedInterval = new List<Interval>();
+                int start = intervals[0].Start, end = intervals[0].End;
+
+                foreach (var interval in intervals)
+                {
+                    if (interval.Start <= end)
+                        end = Math.Max(interval.End, end);
+                    else
+                    {
+                        mergedInterval.Add(new Interval(start, end));
+                        start = interval.Start;
+                        end = interval.End;
+                    }
+                }
+
+                mergedInterval.Add(new Interval(start, end));
+
+                return mergedInterval;
             }
         }
 
@@ -108,6 +218,6 @@ namespace MergeIntervalsPattern
             void IDisposable.Dispose() { }
 
         }
-            
+
     }
 }
